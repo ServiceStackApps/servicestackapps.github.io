edc763514f5082a66068109d677f7dd9780cb11a
Demis Bellot <demis.bellot@gmail.com>
2016-01-24 20:10:23 -0500

    import typescript-redux static files

---
+++ typescript-redux/example03/app.tsx
@@ -0,0 +1,10 @@
+/// <reference path='../../typings/tsd.d.ts'/>
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Counter from './Counter';
+
+ReactDOM.render(<Counter />, document.getElementById("content"));
+
+ 
